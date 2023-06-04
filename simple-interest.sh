44138330b82f7b9603297148848d945e40c9aa1a
willvernon <115375644+willvernon@users.noreply.github.com>
2023-06-03 20:52:01 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,7 +5,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # willvernon
    # Input:
    # p, principal amount
    # t, time period in years
